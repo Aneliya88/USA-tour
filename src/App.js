ce721ce2f2dd4957e87ac6c471e061643163d500
aneliya88 <aneliya.b@yahoo.com>
2024-05-14 19:21:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { data } from './data';
 import './App.css';
 
 function App() {
-
   const [cities, setCities] = useState (data);
   const [showText, setShowText] = useState(false);
   const removeCity = (id) => {
@@ -14,9 +13,6 @@ function App() {
     element.showMore = !element.showMore
     setShowText(!showText)
   }
-
-
-
   return (
     <div className="App">
       <div className='container'>
@@ -24,12 +20,10 @@ function App() {
       </div>
       {cities.map((element => {
         const {id, city, description, image, showMore} = element;
-        
          return ( 
           <div key={id}>
             <div className='container'>
               <h2><span>{id} </span>- {city}</h2>
-             
             </div>
             <div className='container'>
               <p>{showMore ? description : description.substring(0, 220) + "..."}
@@ -37,13 +31,10 @@ function App() {
               </div>
             <div className='container'>
               <img src={image} width="300px"/>
-
             </div>
-           
             <div className='container'>
               <button className='btn' onClick={()=> removeCity(id)}>Remove</button>
             </div>
-
           </div>
          )
       }))}
@@ -53,5 +44,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
